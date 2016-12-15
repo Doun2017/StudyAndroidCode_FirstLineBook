e8e37d89926291809977b0417bf40b4ee0ee9516
Doun <liushi2008wh@163.com>
2016-12-16 01:18:41 +0800

    第一章-完结

--- MyApplicationTest/app/src/main/java/com/example/doun/myapplicationtest/MainActivity.java
+++ MyApplicationTest/app/src/main/java/com/example/doun/myapplicationtest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.doun.myapplicationtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("data", "onCreate execute");
+        Log.d(TAG, "onCreate: executed");
 
     }
+
+    private static final String TAG = "MainActivity";
 
 }
