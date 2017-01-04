73bb3c2e118a60092d9d0ba4aa5816c22cae6121
DounWork <liushi2008wh@163.com>
2017-01-04 17:26:17 +0800

    xxx

--- DatabaseTest/app/src/main/java/com/example/power/databasetest/MainActivity.java
+++ DatabaseTest/app/src/main/java/com/example/power/databasetest/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.power.databasetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private MyDatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 1);
+        Button creatbutton = (Button)findViewById(R.id.button_create);
+        creatbutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                dbHelper.getWritableDatabase();
+            }
+        });
     }
 }
