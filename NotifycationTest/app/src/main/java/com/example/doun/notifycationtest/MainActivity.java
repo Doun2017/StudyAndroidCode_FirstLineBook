b3848c175da5618284d079b7d6ad8013c129bc97
Doun <liushi2008wh@163.com>
2017-01-07 22:59:43 +0800

    c

--- NotifycationTest/app/src/main/java/com/example/doun/notifycationtest/MainActivity.java
+++ NotifycationTest/app/src/main/java/com/example/doun/notifycationtest/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.doun.notifycationtest;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NotificationActivity.class);
+                PendingIntent pi = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
                 NotificationManager manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
                 Notification notification = new NotificationCompat.Builder(MainActivity.this)
                         .setContentTitle("this is title")
@@ -27,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
                         .setWhen(System.currentTimeMillis())
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                        .setContentIntent(pi)
+//                        .setVibrate(new long[]{0,200,200,200})
+                        .setPriority(Notification.PRIORITY_MAX)
+//                        .setLights(Color.RED, 1000, 1000)
+//                        .setAutoCancel(true)
                         .build();
                 manager.notify(1, notification);
             }
