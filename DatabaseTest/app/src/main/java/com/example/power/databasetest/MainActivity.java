40d4b11623edb75d767a02119fa77ea41fb8fe8d
DounWork <liushi2008wh@163.com>
2017-01-09 10:04:25 +0800

    qq

--- DatabaseTest/app/src/main/java/com/example/power/databasetest/MainActivity.java
+++ DatabaseTest/app/src/main/java/com/example/power/databasetest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.power.databasetest;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,12 +15,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 1);
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 2);
+
         Button creatbutton = (Button)findViewById(R.id.button_create);
         creatbutton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 dbHelper.getWritableDatabase();
+            }
+        });
+
+        Button addbutton = (Button)findViewById(R.id.button_add);
+        addbutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("name", "the da vinci code");
+                values.put("author", "dan browm");
+                values.put("pages", 456);
+                values.put("price", 16.25);
+                db.insert("Book", null, values);
+                values.clear();
+                values.put("name", "the lost symbol");
+                values.put("author", "dan browm");
+                values.put("pages", 562);
+                values.put("price", 19.25);
+                db.insert("Book", null, values);
             }
         });
     }
